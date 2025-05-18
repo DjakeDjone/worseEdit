c78b9c0ec0af96fc62abaf8548f619bfc3ee19a8
DjakeDjone <benjamin.friedl@htlstp.at>
2025-05-18 16:36:45 +0200

    feat: update error handling and UI improvements
    - Changed error.vue to use a primary color for the error message section.
    - Added a go back button in notFound.vue for better navigation.
    - Updated nuxt.config.ts to enable devtools and added page transition settings.
    - Enhanced theme colors in maintheme.ts for better visual consistency.
    - Added new Tiptap extensions for improved text editing capabilities.
    - Introduced a logger plugin in server/plugins/logger.ts to log requests and errors.
    - Updated tailwind.config.ts to comment out the theme extension for future adjustments.
    - Updated package.json and pnpm-lock.yaml to include new dependencies and versions.
    - Updated favicon.ico for branding consistency.

---
+++ server/plugins/logger.ts
@@ -0,0 +1,11 @@
+export default defineNitroPlugin((nitro) => {
+
+    nitro.hooks.hook("request", (event) => {
+        console.log("Request received:", event.node.req.method, event.node.req.url);
+        console.log("Request headers:", event.node.req.headers);
+    });
+
+    nitro.hooks.hook("error", (error) => {
+        console.error("Error occurred:", error);
+    });
+});
